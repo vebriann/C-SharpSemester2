e23bb08e928907875868006c117944a1f35989a1
Vebrian Developer <vebrianns01@gmail.com>
2023-01-09 11:45:05 +0700

    update

---
+++ latihanArrayDimensi2/latihanArrayDimensi2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace latihanArrayDimensi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[2, 2];
+
+            Console.Clear();
+            Console.WriteLine("Belajar array dimensi 2 dengan inputan");
+            Console.WriteLine("Menggunakan methode row mayor order \n");
+            Console.WriteLine("Input matriks: ");
+
+            // looping untuk inputan matriks
+            for (int i =0; i < 2; i++)
+            {
+                for (int j =0; j < 2; j++)
+                {
+                Console.Write($"Input Array [{i}][{j}] : ");
+                array[i,j] = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Output Matriks");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine($"Array [{i}][{j}] Bernilai : {array[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ penjumlahanArray/penjumlahanArray/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace penjumlahanArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrik1 = new int[2, 2];
+            int[,] matrik2 = new int[2, 2];
+            int[,] hasil = new int[2, 2];
+            int x, y;
+            Console.Clear();
+
+            // input matrik 1 
+            Console.WriteLine("Input matriks 1");
+            for (int a = 0; a < 2; a++)
+            {
+                for (int b = 0; b < 2; b++)
+                {
+                    Console.Write($"Input nilai matriks [{a}] [{b}] : ");
+                    matrik1[a, b] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            // input matrik 2 
+            Console.WriteLine();
+            Console.WriteLine("Input matriks 2");
+            for (int c = 0; c < 2; c++)
+            {
+                for (int d = 0; d < 2; d++)
+                {
+                    Console.Write($"Input nilai matriks [{c}] [{d}] : ");
+                    matrik2[c, d] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            // cetak matriks 1
+            Console.WriteLine();
+            for (x = 0; x < 2; x++)
+            {
+                for (y = 0; y < 2; y++)
+                {
+                    Console.Write($"Cetak Matriks 1 Baris [{x}] column [{y}] bernilai {matrik1[x, y]}");
+                    Console.WriteLine();
+                }
+            }
+
+            // cetak matriks 2
+            Console.WriteLine();
+            for (x = 0; x < 2; x++)
+            {
+                for (y = 0; y < 2; y++)
+                {
+                    Console.Write($"Cetak Matriks 2 Baris [{x}] column [{y}] bernilai {matrik2[x, y]} ");
+                    Console.WriteLine();
+                }
+            }
+
+
+
+            // hasil perhitungan
+            Console.WriteLine();
+            Console.Write("==== Hasil Penjumlahan matriks ====");
+            Console.WriteLine();
+
+            // proses penjumlahan
+            for (x = 0; x < 2; x++)
+            {
+                for (y = 0; y < 2; y++)
+                {
+                    hasil[x, y] = matrik1[x, y] + matrik2[x, y];
+                }
+            }
+
+            // output hasil
+            for (x = 0; x < 2; x++)
+            {
+                for (y = 0; y < 2; y++)
+                {
+                    Console.Write($"Hasil= {hasil[x, y]} ");
+                }
+                Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
